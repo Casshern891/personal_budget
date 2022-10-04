8f5f876a32ed7fa5e41f8c80eb54cc74dfad69c1
Richard Lovell <cassh3rn@talktalk.net>
2022-10-04 20:12:24 +0100

    set up server to listen at 3000

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.status(200).send("Hello World!");
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}.`);
+});
